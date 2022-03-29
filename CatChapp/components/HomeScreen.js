572f955d527c9329f5d89d985464098df9e6e60d
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2022-03-29 22:32:32 +0200

    Add files via upload

--- CatChapp/components/HomeScreen.js
+++ CatChapp/components/HomeScreen.js
@@ -1,19 +1,37 @@
 // Homes.js
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 export default class HomeScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.textS}>Bonjour,{"\n"} Que souhaitez-vous faire ?</Text>
           <Button
-          title="Go to About"
+          title="CONNEXION"
           onPress={() => this.props.navigation.navigate('About')}
 />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    fontSize: 2,
+    flex: 1,
+    //justifyContent: 'space-between',
+    backgroundColor: '#F9CBCB',
+  },
+  textS: {
+    paddingTop: 50,
+    textAlign: "center",
+    color: 'black',
+    fontSize: 25,
+    paddingBottom: 50,
+  },
+});
+
+//<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
