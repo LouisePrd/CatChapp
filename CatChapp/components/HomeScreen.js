c7899e368f3c1a64847b23154ccac33e653384e1
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2022-03-31 02:20:57 +0200

    home fini

--- CatChapp/components/HomeScreen.js
+++ CatChapp/components/HomeScreen.js
@@ -1,37 +1,66 @@
 // Homes.js
 import React, { Component } from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet, Alert, Image, ImageBackground, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 export default class HomeScreen extends Component {
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textS}>Bonjour,{"\n"} Que souhaitez-vous faire ?</Text>
+        <ImageBackground style={ styles.imgBackground } 
+                 resizeMode='cover' 
+                 source={require('../assets/fond2.png')}>
+        <Text style={styles.textS}>Bienvenue !{"\n"} Que souhaitez-vous faire ?</Text>
           <Button
-          title="CONNEXION"
-          onPress={() => this.props.navigation.navigate('Connexion')}
-/>
+          title="Connexion"
+          color="#4d1a8a"
+          onPress={() => this.props.navigation.navigate('About')}/>
+          <Button
+          title="Explorer"
+          color="#4d1a8a"
+          onPress={() => this.props.navigation.navigate('Connexion')}/>
+          <Image
+          source={require('../assets/cat.gif')}
+          style={{
+            width: 350,
+            height: 250,
+            alignSelf: 'center',
+            marginBottom: 20,
+            marginTop : 20}}
+          />
+          <Button
+          title="En savoir plus"
+          color="#904c8c"
+          onPress={() => this.props.navigation.navigate('About')}/>
+          </ImageBackground>
       </View>
     )
   }
 }
 
+
+
+//Styles
+          //onPress={() => Alert.alert('Simple Button pressed')}
 const styles = StyleSheet.create({
   container: {
     fontSize: 2,
     flex: 1,
-    //justifyContent: 'space-between',
-    backgroundColor: '#F9CBCB',
+    //backgroundColor: '#E7C5E8',
   },
+  imgBackground: {
+    width: '100%',
+    height: '100%',
+    flex: 1 
+},
   textS: {
-    paddingTop: 50,
+    paddingTop: 80,
+    fontWeight: 'bold',
     textAlign: "center",
     color: 'black',
     fontSize: 25,
-    paddingBottom: 50,
-  },
+    paddingBottom: 30,
+  }
 });
-
-//<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
