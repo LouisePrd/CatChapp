cd0cd1478fbe99a08624dbb9caf6d105c5b10ec3
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2022-04-02 14:24:28 +0200

    Update config.js

--- CatChapp/firebase/config.js
+++ CatChapp/firebase/config.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLwpOLaHx4Une0AD4BqNaoaN0lUwqFpHo",
-  authDomain: "catchapp-fd5bc.firebaseapp.com",
-  projectId: "catchapp-fd5bc",
-  storageBucket: "catchapp-fd5bc.appspot.com",
-  messagingSenderId: "873243235593",
-  appId: "1:873243235593:web:44e7cc974c54891fdf2fd8",
-  measurementId: "G-4CZSF5YPFJ"
+
 };
 
 firebase.initializeApp(firebaseConfig);
